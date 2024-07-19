093830b565c1d75ceae27cf5d63b753ffc7b8fad
Lumind312 <ryanlu312@gmail.com>
2024-07-18 18:46:30 -0700

    Added functionality to having a buffer group

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# Given the formula for animals, display animals needed to create
-# Also display when there is a cycle
-# TODO: figure out when to stop displaying, or if I need to continue displaying an animal
-# Also, display cycles if possible. So maybe stop when we hit a cycle?
-
-# 1. Read in excel sheet
-# 2. Save to a list or hash for fast access
-# 3. Get animal to display.
-# 4. Grab animal from hash, get results for child animals. *BFS*
-# 5. Need to know when to stop creating the graph. Currently uses BFS and stops at cycles, but I want it to stop like 3 levels down. Or format it to have levels (rank).
-
 from graphviz import Digraph
 from queue import Queue
+
 def printTree(root, formulas):
 	# https://treelib.readthedocs.io/en/latest/
 	tree = Digraph()
@@ -27,7 +17,7 @@ def printTree(root, formulas):
 		if curr == 'Cats' or curr == 'Dogs':	# any cat/dog applies, so there's no df row for it
 			continue
 		child = (formulas.loc[curr]).to_list()
-		print(child)
+		# print(child)
 		# print(curr)
 		for i in child:
 			if i == 'Hippo':
@@ -36,19 +26,20 @@ def printTree(root, formulas):
 				seen.append(i)
 				next_group.put(i)
 				tree.node(i, i)
-			tree.edge(i, curr)
+			tree.edge(i, curr)					# duplicate lines are ok
 			# print(tree)
 		
 		if q.empty():							# when we use up our main queue, pull from the next group
-			q = Queue(next_group)
-			next_group = Queue()
+			# cannot clone a queue directly
+			while not next_group.empty():
+				q.put(next_group.get())
+	
 	print('Tree has been created.')
-	tree.unflatten().render()
+	tree.unflatten().render()					# save diagram in generated .pdf
 
-
-def readSheet(sheet):	# 1
+def readSheet(sheet):			# 1
 	import pandas as pd
-	df = pd.read_csv(sheet)
+	df = pd.read_csv(sheet)		# 2
 	df.rename(columns=df.iloc[0], inplace=True)
 	df.drop([0], inplace=True)
 	df.dropna(inplace=True)
@@ -62,3 +53,15 @@ df = readSheet('Pixel People Formulas.csv')
 animal = input('Give an animal to display (\'q\' to quit): ')	# 3
 printTree(animal, df)
 print('Thank you.')
+
+
+
+# Given the formula for animals, display animals needed to create
+# Also display when there is a cycle
+# TODO: figure out when to stop displaying, or if I need to continue displaying an animal
+# Also, display cycles if possible. So maybe stop when we hit a cycle?
+
+# 1. Read in excel sheet, save to df
+# 2. Get animal to display.
+# 3. Grab animal from df, get results for child animals. *BFS*
+# 4. Need to know when to stop creating the graph. Currently uses BFS and stops at cycles, but I want it to stop like 3 levels down. Or format it to have levels (rank).
